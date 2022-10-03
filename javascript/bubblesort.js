9050590de6e21457a3eb1d2228ed113597401a68
ayeolakenny <ayeolakenny@gmail.com>
2022-10-03 16:45:41 +0100

    javascript bubble sort

---
+++ javascript/bubblesort.js
@@ -0,0 +1,15 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+};
+
+let arr = [234, 43, 55, 63, 5, 6, 235, 547];
+bubbleSort(arr);
